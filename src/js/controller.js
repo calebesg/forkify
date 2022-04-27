b07bb2008048fd96e748b66f21e02525f28f56d0
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-27 09:03:07 -0300

    testing api connection

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,35 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const getRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await response.json();
+
+    if (response.ok === false)
+      throw new Error(`ERROR: ${data.message}, STATUS: ${response.status}`);
+
+    const { recipe: dataRecipe } = data.data;
+
+    const recipe = {
+      id: dataRecipe.id,
+      title: dataRecipe.title,
+      cookingTime: dataRecipe.cooking_time,
+      publisher: dataRecipe.publisher,
+      servings: dataRecipe.servings,
+      ingredients: dataRecipe.ingredients,
+      imageUrl: dataRecipe.image_url,
+      SouceUrl: dataRecipe.source_url,
+    };
+
+    console.log(recipe);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+getRecipe();
