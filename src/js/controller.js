cda6ef88bcf052aa85fbcf14c306e0b5fd70bb67
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-28 10:57:25 -0300

    implemented publisher subscribe pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -18,6 +18,7 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
