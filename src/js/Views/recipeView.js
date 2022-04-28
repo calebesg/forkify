baac0d6af912e91ce0ec01ba6d7fe62f1573631f
CalebeSG <calebeguimaraes10@gmail.com>
2022-04-28 09:33:34 -0300

    added fractional lib

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,3 +1,4 @@
+import { Fraction } from 'fractional';
 import icons from 'url:../../img/icons.svg';
 
 class RecipeView {
@@ -121,7 +122,9 @@ class RecipeView {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity || 0}</div>
+        <div class="recipe__quantity">${
+          ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
